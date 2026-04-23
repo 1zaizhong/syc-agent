5336eb9870865a9e5433815ad944a4b2a3c715c9
zai_zhong_ <zai_zhong_@noreply.gitcode.com>
2026-04-23 20:26:54 +0800

    数据库，redis配置

---
+++ src/main/java/com.syc.agent/SalesAgentApplication.java
@@ -0,0 +1,17 @@
+package com.syc.agent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 粟英朝
+ * @version 0.0.3
+ * @date 2026/4/23 下午6:19
+ * @description
+ **/
+@SpringBootApplication
+public class SalesAgentApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SalesAgentApplication.class, args);
+    }
+}
